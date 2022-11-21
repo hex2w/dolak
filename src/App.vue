75fe34436e2b8da1e215ff0bdad57b2d1f8dd8a2
hex3928 <n7f@protonmail.com>
2022-11-21 21:00:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import emojiUrl from "./assets/emoji.jpeg"
+</script>
+
+<template>
+<section class="mt-10 min-h-screen flex flex-col justify-center text-center">
+
+<h1 class="text-2xl py-1 mb-10 text-pink-500 bg-black">VAZENI DOLAK! ANTI KK ANTI MM</h1>
+
+<h2 class="text-xl text-purple-900 mb-2">PODPOR DOLAK V PRAZE ZUSTAT! DOLAK! NAJDI UBYTKO PRO KYNIKA!!!</h2>
+<iframe class="mx-auto mb-10 rounded-md" width="560" height="315"
+    src="https://www.youtube.com/embed/U8a-I58cXvQ" title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+
+<p class="text-sm py-2 max-w-prose mx-auto">Srdcervoucí příběh oběti naší doby. Teprve 19 letý mladík Antonín Dolák byl v noci z 9.
+    na 10. listopadu napaden tepláčkami míťačkami když se pokoušel v klidu relaxovat. Prosím, rozšiřme povědomí
+    o tepláčkách míťačkách, aby nedošlo k dalším takovým útokům.</p>
+<div class="mx-auto">
+    <blockquote
+        class="instagram-media"
+        data-instgrm-captioned
+        data-instgrm-permalink="https://www.instagram.com/reel/Cku6sSJgUhR/?utm_source=ig_embed&amp;utm_campaign=loading"
+        data-instgrm-version="14"
+    ></blockquote>
+</div>
+
+</section>
+<section class="mt-10 p-5">
+
+<div class="inline-flex gap-2">
+    <p>Kontakt pan DOlak</p>
+    <img :src="emojiUrl" alt="emoji" />
+</div>
+<a class="underline hover:no-underline text-blue-700" href="mailto:dolakantonin@gmail.com">dolakantonin@gmail.com</a>
+<p class="mb-10 mx-5 text-red-500 bg-black">U{POZORNENI!!} DOLAK NECHATUJE.! POUZE SRAZY!</p>
+
+<div class="mx-auto">
+    <blockquote
+        class="instagram-media"
+        data-instgrm-captioned
+        data-instgrm-permalink="https://www.instagram.com/reel/CklBEdfAKUN/?utm_source=ig_embed&amp;utm_campaign=loading"
+        data-instgrm-version="14"
+    ></blockquote>
+</div>
+<p class="text-center font-bold text-purple-900">DOLAK SWAG!! NECHAPES BEJB!!</p>
+
+</section>
+</template>
